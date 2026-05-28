112f8bc1a35f1ad4e5c6db751623b25e2d44f044
N-Dave-08 <cdave.dev@gmail.com>
2026-05-28 09:58:22 +0800

    feat: implement geolocation and map setup in main.js

--- 03_google-maps-clone/main.js
+++ 03_google-maps-clone/main.js
@@ -1,9 +1,33 @@
 // sets the access token, associating the map with your Mapbox account and its permissions
-mapboxgl.accessToken = "your_access_token"; // get it from your mapbox account
+mapboxgl.accessToken = "YOUR_MAPBOX_ACCESS_TOKEN"; // get your mapbox accessToken from your account
 
-// creates the map, setting the container to the id of the div you added in step 2, and setting the initial center and zoom level of the map
-const map = new mapboxgl.Map({
-  container: "map", // container ID
-  center: [-71.06776, 42.35816], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-  zoom: 9, // starting zoom
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true,
 });
+
+function successLocation(position) {
+  console.log(position);
+  setupMap([position.coords.longitude, position.coords.latitude]);
+}
+
+function errorLocation() {
+  setupMap([-2.24, 53.48]);
+}
+
+function setupMap(center) {
+  // creates the map, setting the container to the id of the div you added in step 2, and setting the initial center and zoom level of the map
+  const map = new mapboxgl.Map({
+    container: "map", // container ID
+    center: center, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    zoom: 9, // starting zoom
+  });
+
+  const nav = new mapboxgl.NavigationControl();
+  map.addControl(nav, "top-left");
+
+  const directions = new MapboxDirections({
+    accessToken: "YOUR_MAPBOX_ACCESS_TOKEN", // get your mapbox accessToken from your account
+  });
+
+  map.addControl(directions, "top-right");
+}
