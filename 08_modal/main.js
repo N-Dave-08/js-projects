56e873c5f1e90d409276336f89897bde0ff9f3a0
N-Dave-08 <cdave.dev@gmail.com>
2026-06-22 21:29:36 +0800

    feat: add login modal for testing

--- 08_modal/main.js
+++ 08_modal/main.js
@@ -2,13 +2,6 @@ const openModalButtons = document.querySelectorAll("[data-modal-target]");
 const closeModalButtons = document.querySelectorAll("[data-close-button]");
 const overlay = document.getElementById("overlay");
 
-openModalButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const modal = document.querySelector(button.dataset.modalTarget);
-    openModal(modal);
-  });
-});
-
 overlay.addEventListener("click", () => {
   const modals = document.querySelectorAll(".modal.active");
   modals.forEach((modal) => {
@@ -16,10 +9,10 @@ overlay.addEventListener("click", () => {
   });
 });
 
-closeModalButtons.forEach((button) => {
+openModalButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    const modal = button.closest(".modal");
-    closeModal(modal);
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
   });
 });
 
@@ -28,6 +21,13 @@ function openModal(modal) {
   modal.classList.add("active");
   overlay.classList.add("active");
 }
+
+closeModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = button.closest(".modal");
+    closeModal(modal);
+  });
+});
 
 function closeModal(modal) {
   if (modal == null) return;
